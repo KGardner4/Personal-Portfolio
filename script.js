3ad4b604685a2eefc20f17a4d792f184eaf8cce7
DePaul <CONF0577@depaul.edu>
2018-07-24 15:38:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready (function() }
+	
+	<button onclick="Zodiac Sign()">Try it</button>
+
+<script>
+function myFunction() {
+    window.open("https://www.astrology.com/astrology-101/zodiac-signs/virgo/");
+}
+</script>
+	
+	
+	
+	
+	
+	}};
+	
